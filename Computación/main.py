43c192329c65175e886277c89a949f7a983495c2
Lina Marcela Giraldo-Murillo <50031393+linagiraldom@users.noreply.github.com>
2022-12-01 17:45:32 -0500

    Documentation update

--- Computación/main.py
+++ Computación/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static") ###
 
 #file='https://raw.githubusercontent.com/restrepo/OpenAlexGroup/main/data/calificaciones.json'
-file='https://raw.githubusercontent.com/linagiraldom/Parcial2/main/static/data/FACom_art.json'
+file='https://raw.githubusercontent.com/linagiraldom/Parcial2/main/Computación/static/data/FACom_art.json'
 
 
 #JSON SCHEME
@@ -33,13 +33,23 @@ file='https://raw.githubusercontent.com/linagiraldom/Parcial2/main/static/data/F
 
 @app.get("/")
 def read_item(affiliation	: str = ""):
-    '''
-    You can write the API documentation here:
-    
-    For example: 
-    
-    USAGE: http://127.0.0.1:8000/?student_id=1113674432
-    '''
+	"""
+    Esta aplicación permite consultar los artículos asociados a una institución basándose en una palabra clave,
+    la cual es ingresada como un parámetro de URL.
+	
+	Ejemplo de uso
+	--------------
+
+	Ingresar por url: http://127.0.0.1:8000/ devolverá una lista de todos los artículos que estén asociados a
+	una institución en la base de datos.
+
+	Ingresar parámetro por URL: http://127.0.0.1:8000/?affiliation=nombre%20de%20institución devolverá todos
+	los artículos donde al menos uno de los autores esté asociado a una institución que en su nombre lleve
+	el valor del parámetro `affiliation`.
+
+	El parámetro `affiliation` puede ser una palabra clave, como facom. No es case-sensitive, por lo que 
+	http://127.0.0.1:8000/?affiliation=facom equivale a http://127.0.0.1:8000/?affiliation=FACom
+    """
     
     #Real time JSON file
     r=requests.get(file)
